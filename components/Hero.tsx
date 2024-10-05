9b081a833fa5be2a6c49a3977b18268bf97733e6
Solutech11 <84994162+Solutech11@users.noreply.github.com>
2024-10-05 11:23:50 +0100

    update

---
+++ components/Hero.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomButton } from "@components";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+        Find, book, rent or meet a celebrity—quick and super easy!
+        </h1>
+
+        <p className="hero__subtitle">
+          Streamline your celebrity rental experience with our effortless booking
+          process.
+        </p>
+
+        <CustomButton
+          title="Explore Celebs"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/81111.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
